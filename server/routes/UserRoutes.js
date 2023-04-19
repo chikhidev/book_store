1074695ad41bae0670fbcbc9df2cb80749ef00be
abderrahim chikhi <chikhi.dev@gmail.com>
2023-04-19 00:58:34 +0000

    validation added

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -6,11 +6,11 @@ const UserRouter = express.Router();
 //define user routes
 UserRouter.get("/", Controller.User.findByEmail)
 
-UserRouter.get("/create",
+UserRouter.post("/create",
                 Controller.User.createUser
                 ) //this a private route it requires a password
 
-UserRouter.get("/drop",
+UserRouter.delete("/drop",
                 Controller.User.dropUser
                 ) //this a private route it requires a password
 
