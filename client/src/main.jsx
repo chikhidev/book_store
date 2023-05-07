8cf936025ef5b40c9b310b1e9e5127cb06a0b6a3
gouzidev <gouzi.dev@gmail.com>
2023-05-07 12:20:43 +0100

    minor fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 
+// REDUX
+
+import { Provider } from 'react-redux';
+
+import store from '../src/redux/store';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+
+    <React.StrictMode>
+
+      <App />
+
+    </React.StrictMode>
+
+  </Provider>,
 )
