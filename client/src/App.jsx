e7811d6d3dae9aabb1ee615612b20b894b2cedeb
gouzidev <gouzi.dev@gmail.com>
2023-05-08 23:59:27 +0100

    well, read the branch name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,52 @@ import './css/index.css';
 import './css/hero.css';
 import './js/index.js';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Register from './components/Register';
 import FeaturedSection from './components/FeaturedSection';
 const LoginContext = createContext();
+import store from './redux/store';
+
+function App() {
+  const [isLogged, setIsLogged] = useState(store.getState().loginStatus);
+  setInterval(() => {
+    
+  }, 1000)
+  const checkAlreadyLogged = () => {
+    if (localStorage.getItem("token")) {
+      setIsLogged(true);
+    } else {
+      setIsLogged(false);
+    }
+  }
+
+  useEffect(() => {
+    checkAlreadyLogged();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setIsLogged(store.getState().loginStatus);
+    });
+    return unsubscribe;
+  }, []);
+
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/categories" element={<About/>} />
+            <Route path="/new" element={<Contact/>} />
+            <Route path="login" element={isLogged ? <Navigate to="/"/> : <Login />} />    {/* If user is logged in, then redirect to home page, else go to login page */}
+            <Route path="/register" element={isLogged ? <Navigate to="/"/> : <Register />} />    {/* If user is logged in, then redirect to home page, else go to login page */}
+          </Routes>
+        </div>
+      </Router>
+  );
+}
 
 function Home() {
   return (
@@ -37,36 +77,5 @@ function Contact() {
   );
 }
 
-function App() {
-  const [isLogged, setIsLogged] = useState(false);
-
-  const checkAlreadyLogged = () => {
-    if (localStorage.getItem("token")) {
-      setIsLogged(true);
-    } else {
-      setIsLogged(false);
-    }
-  }
-
-  useEffect(() => {
-    checkAlreadyLogged();
-  }, []);
-    return (
-    <LoginContext.Provider value={[isLogged, setIsLogged]} >
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/categories" element={<About/>} />
-            <Route path="/new" element={<Contact/>} />
-            <Route path="login" element={isLogged ? <Navigate to="/"/> : <Login />} />    {/* If user is logged in, then redirect to home page, else go to login page */}
-            <Route path="/register" element={isLogged ? <Navigate to="/"/> : <Register />} />    {/* If user is logged in, then redirect to home page, else go to login page */}
-          </Routes>
-        </div>
-      </Router>
-    </LoginContext.Provider>
-  );
-}
 
 export {App, LoginContext};
