155822daa3e08aed7fee72ca50359f81f6e846fd
gouzidev <gouzi.dev@gmail.com>
2023-05-19 00:06:36 +0100

    fixed some minor issues, need to add isFav on every book,

--- client/src/components/Favorite.jsx
+++ client/src/components/Favorite.jsx
@@ -1,43 +1,38 @@
 import { useState, useEffect } from 'react';
-import { useSelector , useDispatch } from 'react-redux';
-import store from '../redux/store';
+import { useSelector, useDispatch } from 'react-redux';
 import { LOGIN, LOGOUT, TOGGLE_BOOK_FAV } from '../redux/actions';
 import Card from './Card';
 
 const Favorite = () => {
   const [fetchedBooks, setFetchedBooks] = useState([]);
+  const favBooks = useSelector((state) => state.favBooks); // Accessing favBooks from Redux store
+  const dispatch = useDispatch();
 
   const fetchBooks = async () => {
-    const bookIds = store.getState().favBooks; // Assuming you have favBooks stored in your Redux store
-  
-    const promises = bookIds.map(async (id) => {
+    const promises = favBooks.map(async (id) => {
       const response = await fetch(`http://localhost:4000/book/${id}`);
       const data = await response.json();
-      let book = data.data.book;
+      const book = data.data.book;
       return book;
     });
-  
+
     const books = await Promise.all(promises);
     setFetchedBooks(books);
-    console.log(books);
-    
   };
-  
+
   useEffect(() => {
     fetchBooks();
-  }, []);
+  }, [favBooks]); // Re-fetch books when favBooks changes in the Redux store
 
-  // Rest of your component code
-    return (
-        <div className='favorite-books'>
-        {fetchedBooks.length > 0 ?
-          fetchedBooks.map(book => {
-            return (<Card book={book} key={book._id} />)
-          })
-        :
-        ""
-        }
-      </div>
-    )
-}
-export default Favorite
+  return (
+    <div className='favorite-books'>
+      {fetchedBooks.length > 0 ? (
+        fetchedBooks.map((book) => <Card book={book} key={book._id} />)
+      ) : (
+        <h1>Loading Books</h1>
+      )}
+    </div>
+  );
+};
+
+export default Favorite;
