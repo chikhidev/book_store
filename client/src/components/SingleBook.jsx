ab8f874c87fbbdb994dc27f65cbd882b7c2bbe6f
gouzidev <gouzi.dev@gmail.com>
2023-06-04 00:51:05 +0100

    added z3ter dyal abderrahim hit howa m3gaz mabghach ykhdm kaykhdmni o kaynguer 3la design dyali fhali howa dar chi haja fl front end

--- client/src/components/SingleBook.jsx
+++ client/src/components/SingleBook.jsx
@@ -14,7 +14,9 @@ import CategorySlider from './CategorySlider.jsx';
 import BookCard from './BookCard.jsx';
 import MoreLikeThis from "./MoreLikeThis"
 
+
 const SingleBook = () => {
+
     const { id } = useParams();
     const [isFetchRunned, setIsFetchRunned] = useState(false);
     const [fetchedBook, setFetchedBook] = useState(false);
@@ -26,39 +28,7 @@ const SingleBook = () => {
     const [loading, setLoading] = useState(false)
     const [errorMsg, setErrorMsg] = useState("")
     const [orderMsg, setOrderMsg] = useState("")
-
-    const showShipping = () => {
-        Swal.fire({
-            title: 'Shipping Informations : ',
-            html: `<input type="text" id="addressLine1" class="swal2-input" placeholder="adress line 1">
-            <input type="text" id="city" class="swal2-input" placeholder="city">
-            <input type="text" id="state" class="swal2-input" placeholder="state">
-            <input type="text" id="province" class="swal2-input" placeholder="province">
-            <input type="text" id="country" class="swal2-input" placeholder="country">`,
-            confirmButtonText: 'Sign in',
-            focusConfirm: false,
-            preConfirm: () => {
-              const addressLine1 = Swal.getPopup().querySelector('#addressLine1').value
-              const city = Swal.getPopup().querySelector('#city').value
-              const state = Swal.getPopup().querySelector('#state').value
-              const province = Swal.getPopup().querySelector('#province').value
-              const country = Swal.getPopup().querySelector('#country').value
-              if (!addressLine1 || !city || !state || !province || !country) {
-                Swal.showValidationMessage(`Please enter valid shipping info`)
-              }
-              return { addressLine1: addressLine1, city: city, state: state, province: province, country: country }
-            }
-          }).then((result) => {
-            Swal.fire(`
-              AddressLine1: ${result.value.addressLine1}
-              City: ${result.value.city}
-              State: ${result.value.state}
-              Province: ${result.value.province}
-              Country: ${result.value.country}
-              Password: ${result.value.password}
-            `.trim())
-          })
-    }
+    
     const orderBook = async () => {
         setLoading(true)
         let response = await fetch(`${ENDPOINT}/order`, {
@@ -79,7 +49,6 @@ const SingleBook = () => {
             })
         })
         let data = await response.json()
-        console.log(data.sucess);
         setLoading(false)
         setOrderSuccess(data.success)
         if (!data.success)
@@ -128,92 +97,96 @@ const SingleBook = () => {
             getBook();
         }
     }, [id]);
-
     return (
         <>
-            {isFetchRunned ? (
-                isBookFetched && fetchedBook ? (
-                    <div key={fetchedBook._id} 
-                    className="book-single-container">
-                        <div className="book-single-row-one">
-                            {/* img */}
-                            <div className="book-single-col-one">
-                                <img className="book-single-imageUrl" src={"http://localhost:4000" + fetchedBook.imageUrl} />
+        {isFetchRunned ? (
+            isBookFetched && fetchedBook ? (
+            <>
+                <section className="pt-12 pb-24 bg-blueGray-100 rounded-b-10xl overflow-hidden">
+                    <div className="container px-4 mx-auto">
+                    <div className="flex flex-wrap justify-center ms-4">
+                        <div className="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
+                            <div className="flex -mx-4 flex-wrap items-center justify-between lg:justify-start lg:items-start xl:items-center">
+                                <div className="w-full sm:w-9/12 px-4">
+                                    <img className="mb-5" src={"http://localhost:4000" + fetchedBook.imageUrl} alt="" />
+                                </div>
                             </div>
-                            {/* book details */}
-                            <div className="book-single-col-two">
-                                <div className="book-single-title">{fetchedBook.title}</div>
-                                <div className="book-single-author">{fetchedBook.author}</div>
-                                <div className="book-single-categories">
-                                    {gotCats && cats.length > 0 ? "Categories : " : ""}
+                        </div>
+                        <div className="w-full lg:w-1/2 px-4">
+                        <div className="max-w-md mb-6">
+                            <span className="text-xs text-gray-400 tracking-wider">{fetchedBook._id}</span>
+                            <h2 className="mt-6 mb-4 text-4xl md:text-5xl lg:text-7xl font-heading font-medium">{fetchedBook.title}</h2>
+                            <p className="flex items-center mb-6">
+                            <span className="mr-2 text-sm text-blue-500 font-medium">$</span>
+                            <span className="text-3xl text-blue-500 font-medium">{fetchedBook.price}</span>
+                            </p>
+                            <p className="text-lg text-gray-400">
+                                {fetchedBook.description}
+                                {gotCats && cats.length > 0 ? "Categories : " : ""}
                                     {gotCats && cats.length == 1 ? "Category : " : ""}
                                     {
-                                      gotCats ? 
-                                          cats.length > 0 ? (
+                                        gotCats ? 
+                                            cats.length > 0 ? (
                                             cats.map((category) => 
                                                 <span onClick={() => <Navigate to={"to"} />} className='book-single-category'>
-                                                      {(category.name + " ")}
+                                                        {(category.name + " ")}
                                                 </span>
                                             )
-                                          ) : <p>Categories : unknown</p>
-                                      : <i>Loading</i>
-                                    }
-                                    
-                                </div>
-                                {/* cta = call to action */}
-                                <div className="book-single-cta">
-                                    <div className="book-single-cta-info cta-section-1">
-                                        <div className="book-single-publish-details">
-                                            <div className="book-single-cta-book-type">
-                                                PaperBack
-                                            </div>
-                                            <div className="book-single-cta-publisher">
-                                                {fetchedBook.publisher} 
-                                            </div>
-                                        </div>
-                                        <div className="book-single-cta-publication-date">
-                                            {getHumanDate(fetchedBook.publicationDate)}
-                                        </div>
-                                    </div>
-                                    <div className="book-single-cta-buy cta-section-2">
-                                        <div className="book-single-cta-buy-price">
-                                            ${fetchedBook.price}
-                                        </div>
-                                        <div className="qte-container">
-                                            <div className="qte-plus" onClick={incrementQte}>+</div>
-                                                <div className="qte">{qte}</div>
-                                            <div className="qte-minus" onClick={decrementQte}>-</div>
-                                        </div>
-                                        <button className={`${loading ? "loading-btn" : ""} book-single-cta-buy-btn`}
-                                            onClick={showShipping}>
-                                            {!loading && "Buy Now"}
-                                        </button>
-                                    </div>
-                                </div>
+                                            ) : <p>Categories : unknown</p>
+                                        : <i>Loading</i>
+                                }
+                            </p>
+                        </div>
+                        <div className="mb-10">
+                            <h4 className="mb-3 font-heading font-medium">Qty:</h4>
+                            <input 
+                                value={qte} 
+                                onChange={(e) => setQte(e.target.value)} className="w-24 px-3 py-2 text-center bg-white border-2 border-blue-500 outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-xl" type="text" placeholder="1" />
+                        </div>
+                        <div className="flex flex-wrap -mx-2 mb-12">
+                            <div className="w-full md:w-2/3 px-2 mb-2 md:mb-0">
+                                <a className={`${loading ? "loading-btn" : ""} block py-4 px-2 leading-8 font-heading font-medium tracking-tighter text-xl text-white text-center bg-blue-500 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 hover:bg-blue-600 rounded-xl`} href="#"
+                                onClick={orderBook}>
+                                    {!loading && "Buy Now"}
+                                </a>
+                            </div>
+                            <div className="w-full md:w-1/3 px-2">
+                            <a className="flex w-full py-4 px-2 items-center justify-center leading-8 font-heading font-medium tracking-tighter text-xl text-center bg-white focus:ring-2 focus:ring-gray-200 focus:ring-opacity-50 hover:bg-opacity-60 rounded-xl" href="#">
+                                <span className="mr-2">Wishlist</span>
+                                <svg width="23" height="22" viewbox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.3235 20.1324L2.52488 10.8515C1.75232 10.0706 1.24237 9.06367 1.06728 7.97339C0.8922 6.88311 1.06086 5.76477 1.54936 4.7768V4.7768C1.91837 4.03089 2.45739 3.3843 3.12201 2.89033C3.78663 2.39635 4.55781 2.06911 5.37203 1.93558C6.18626 1.80205 7.0202 1.86605 7.80517 2.1223C8.59013 2.37855 9.30364 2.81972 9.88691 3.40946L11.3235 4.86204L12.7601 3.40946C13.3434 2.81972 14.0569 2.37855 14.8419 2.1223C15.6269 1.86605 16.4608 1.80205 17.275 1.93558C18.0892 2.06911 18.8604 2.39635 19.525 2.89033C20.1897 3.3843 20.7287 4.03089 21.0977 4.7768V4.7768C21.5862 5.76477 21.7549 6.88311 21.5798 7.97339C21.4047 9.06367 20.8947 10.0706 20.1222 10.8515L11.3235 20.1324Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                </svg>
+                            </a>
                             </div>
                         </div>
-                        <div className="book-single-row-two">
-                            <div className="book-single-details-title">About <i>{fetchedBook.title}</i></div>
-                            <div className="book-single-details-description">{fetchedBook.description}</div>
+                        <div>
+                            <button className="flex w-full pl-6 lg:pl-12 pr-6 py-4 mb-4 justify-between items-center leading-7 rounded-2xl border-2 border-blueGray-200 hover:border-blueGray-300">
+                            <h3 className="text-lg font-heading font-medium">Shipping &amp; returns</h3>
+                            <span>
+                                <svg width="12" height="8" viewbox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.4594 0.289848C10.8128 -0.096616 11.3841 -0.096616 11.7349 0.289848C12.0871 0.676312 12.0897 1.30071 11.7349 1.68718L6.63794 7.21015C6.28579 7.59662 5.71584 7.59662 5.36108 7.21015L0.264109 1.68718C-0.0880363 1.30215 -0.0880363 0.676312 0.264109 0.289848C0.617558 -0.096616 1.18882 -0.096616 1.53966 0.289848L6.00147 4.81927L10.4594 0.289848Z" fill="black"></path>
+                                </svg>
+                            </span>
+                            </button>
                         </div>
-                        <div className="book-single-row-three">
-                        
                         </div>
                     </div>
-                ) : (
+                    </div>
+                </section>
+                <div className="more-books-container">
+                    <span>more like <i><b>{fetchedBook.title}</b></i></span>
+                            <MoreLikeThis cats={cats} />
+                </div>
+            </>
+            ) : (
                     <span>Book doesn't exist</span>
                 )
             ) : (
                 <ThreeDotsWave />
-                
-            )}
             
-            <div className="more-books-container">
-            <span>more like <i><b>{fetchedBook.title}</b></i></span>
-                    <MoreLikeThis cats={cats} />
-            </div>
+        )}
         </>
     );
 };
 
-export default SingleBook;
+export default SingleBook
