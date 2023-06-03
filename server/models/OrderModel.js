562a3b197d3fe605600f91ee59633c9aa4e375ae
chikhidev <chikhi.dev@gmail.com>
2023-06-03 23:48:12 +0100

    dakchi li zwin dyali

--- server/models/OrderModel.js
+++ server/models/OrderModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const orderSchema = new Schema({
   customer: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   book: { type: Schema.Types.ObjectId, ref: 'Book', required:true },
-  qte: {type: Number, required:true, default: 1},
+  qte: {type: Number, required:true, default: 1, min: 0},
   status: { type: String, default: 'pending', required:true },
   note: { type: String, default: 'This order will be sent to the seller\nPaiement when recieving' },
   date: { type: Date, default: Date.now },
